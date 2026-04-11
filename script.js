a22abaaeb914e0667be714d5f37e183aff7549f1
centrixcpl05-design <centrixcpl05@gmail.com>
2026-04-11 16:55:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 window.addEventListener("scroll", function() {
-  let cards = document.querySelectorAll(".card");
+  let cards = document.querySelectorAll(".section-card");
+
   cards.forEach(card => {
     let position = card.getBoundingClientRect().top;
     let screen = window.innerHeight;
 
-    if(position < screen - 100){
+    if (position < screen - 100) {
       card.style.opacity = "1";
       card.style.transform = "translateY(0)";
     }
